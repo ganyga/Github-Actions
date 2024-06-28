3b28074ba72040aed91db6d93ef966e8f9e049b0
ganyga <artzana03@gmail.com>
2024-06-28 18:05:40 +0900

    Slack + AroCD 연동 확인

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Slack Notification Test!!'
+    return 'Slack Notification Test'
 
 if __name__ == '__main__':
     app.run()
